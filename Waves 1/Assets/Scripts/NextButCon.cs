b65f45f4d840abe9cf060a176fb6cce711d3aa12
Fair <541077827@qq.com>
2017-01-22 14:45:22 +0800

    +1

---
+++ Waves 1/Assets/Scripts/NextButCon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NextButCon : MonoBehaviour {
+
+	GameController gc;
+
+
+	void Start () {
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+
+		gc = GameObject.Find ("GameController").GetComponent<GameController> ();
+	}
+
+	void TaskOnClick(){
+		gc.loadNextScene();
+	}
+}
